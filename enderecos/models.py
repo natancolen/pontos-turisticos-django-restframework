ebcb26f19a9b825ff05e36daf94d89076d923bf7
natancolen <natancolen@gmail.com>
2022-09-18 21:17:35 -0300

    criando e modelando a app de localizacao

---
+++ enderecos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Endereco(models.Model):
+    linha1 = models.CharField(max_length=150)
+    linha2 = models.CharField(max_length=150, null=True,blank=True)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=70)
+    pais = models.CharField(max_length=150)
+    latitude = models.IntegerField(null=True,blank=True)
+    longitude = models.IntegerField(null=True,blank=True)
