cc802c9d51f6af48e76d786a9514be4a573c3b9e
natancolen <natancolen@gmail.com>
2022-09-22 19:48:21 -0300

    api enderecos

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from  rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
-from  atracoes.api.viewsets import AtracaoViewSet
+from atracoes.api.viewsets import AtracaoViewSet
+from enderecos.api.viewsets import EnderecoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracaoViewSet)
+router.register(r'enderecos', EnderecoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
