f81a1df2ec97c73d1ec998774c914765169b358e
natancolen <natancolen@gmail.com>
2022-09-27 10:44:05 -0300

    api comentarios

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -19,11 +19,13 @@ from  rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
+from comentarios.api.viewsets import ComentarioViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracaoViewSet)
 router.register(r'enderecos', EnderecoViewSet)
+router.register(r'comentarios', ComentarioViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
